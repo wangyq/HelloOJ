66cf9240cb18fd615642cea62f4e63fe3a5e78f8
Yinqing Wang <yinqingwang@gmail.com>
2015-04-25 09:00:34 +0800

    Update PhoneNumber.java

--- HelloOJ/src/com/siwind/interview/PhoneNumber.java
+++ HelloOJ/src/com/siwind/interview/PhoneNumber.java
@@ -7,20 +7,20 @@ import java.util.Stack;
 /**
  * Created by wang on 2015/4/24.
 
- ÃèÊö
+ æè¿°
  The numberic keypad on your mobile phone looks like below:
 
  1 2 3
  4 5 6
  7 8 9
- 0
+   0
  Suppose you are holding your mobile phone with single hand. Your thumb points at digit 1. Each time you can 1) press the digit your thumb pointing at, 2) move your thumb right, 3) move your thumb down. Moving your thumb left or up is not allowed.
 
  By using the numeric keypad under above constrains, you can produce some numbers like 177 or 480 while producing other numbers like 590 or 52 is impossible.
 
  Given a number K, find out the maximum number less than or equal to K that can be produced.
 
- ÊäÈë
+ è¾“å…¥
  The first line contains an integer T, the number of testcases.
 
  Each testcase occupies a single line with an integer K.
@@ -31,15 +31,15 @@ import java.util.Stack;
 
  For 100% of the data, 1 <= K <= 10500, t <= 20.
 
- Êä³ö
+ è¾“å‡º
  For each testcase output one line, the maximum number less than or equal to the corresponding K that can be produced.
 
- ÑùÀıÊäÈë
+ æ ·ä¾‹è¾“å…¥
  3
  25
  83
  131
- ÑùÀıÊä³ö
+ æ ·ä¾‹è¾“å‡º
  25
  80
  129
